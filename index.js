b7658f34710a3a51ff408ec90a2355fcc5ffb285
Tiffany Crooks <tiffcrooks2011@gmail.com>
2023-04-02 18:48:39 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+{let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2
+}
 
-// Write your code here
+let random = Math.floor(Math.random()*10) + 1
+
+{let num3=22
+let num4=9
+let mod=22%9}
+
+let max=Math.max(1,5,10,15,20)
